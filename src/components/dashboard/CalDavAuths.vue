bdf44ef420bcc2074419bd499e8924f4abcc7dcc
Anton Dachauer <ad@var-lab.com>
2025-06-12 20:33:14 +0200

    Manage CalDav Auths: List

--- src/components/dashboard/CalDavAuths.vue
+++ src/components/dashboard/CalDavAuths.vue
@@ -1,26 +1,68 @@
 <template>
   <h2 class="mb-3">
-    {{ $t('account.headline') }}
-    <font-awesome-icon icon="angle-right" class="small" />
+    <router-link
+      to="/dashboard/account"
+      class="text-decoration-none text-dark"
+    >
+      {{ $t('account.headline') }}
+    </router-link>
+    <font-awesome-icon
+      icon="angle-right"
+      class="small"
+    />
     {{ $t('account.caldav_auths.headline') }}
   </h2>
 
-  <div v-if="loading">
-    <div class="text-center p-5 m-5">
-      <div class="spinner-border text-dark"></div>
-    </div>
-  </div>
+  <div
+    v-if="loading"
+    class="spinner-border text-dark"
+    role="status"
+  ></div>
 
   <div
-      v-else
+    v-else
   >
-
+    <div
+      v-for="auth in auths"
+      :key="auth.id"
+    >
+      <CalDavAuth :auth="auth" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {CalDavAuths} from "../../types/CalDavAuth";
+import CalDavAuth from './CalDavAuth.vue'
+import {getCalDavAuths} from "../../services/caldav";
+import Appointment from "./Appointment.vue";
 
 const loading = ref(true)
+const auths = ref<CalDavAuths>([]);
+
+onMounted(async () => {
+  loadData(true);
+});
+
+function loadData(showLoader: boolean = false) {
+  try {
+    if (showLoader) {
+      loading.value = true;
+    }
+
+    getCalDavAuths()
+        .then((data) => {
+          auths.value = data
+        })
+        .finally(() => {
+          if (showLoader) {
+            loading.value = false
+          }
+        });
+  } catch (error) {
+    console.error("Loading CalDAVAuths failed:", error);
+  }
+}
 </script>
